88ba6acd7009416059d5c127e9e3594739a31a35
Nkazimulo <nkazimuna@gmail.com>
2019-06-11 09:54:13 -0700

    Weekend 11/06/2019

--- app/src/main/java/com/example/fah_fee/WalletActivity.java
+++ app/src/main/java/com/example/fah_fee/WalletActivity.java
@@ -2,12 +2,22 @@ package com.example.fah_fee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class WalletActivity extends AppCompatActivity {
+
+    TextView winnings_txt, balance_txt;
+    Button history_btn, withdraw_btn, top_up_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wallet);
+        winnings_txt = (TextView)findViewById(R.id.wins_txt);
+        balance_txt = (TextView)findViewById(R.id.balance_txt);
+        top_up_btn = (Button)findViewById(R.id.top_up);
+        withdraw_btn = (Button)findViewById(R.id.withdraw);
+        history_btn = (Button)findViewById(R.id.history);
     }
 }
